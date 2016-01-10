fa1c6e4bfc2ea315e7ddd182beb858513fb12dad
alimy <alimy@gility.net>
2016-01-10 22:12:34 +0800

    release-2.2.7 [2012-07-11T16:51:55Z]

--- src/common/parameter.cpp
+++ src/common/parameter.cpp
@@ -175,13 +175,13 @@ namespace tfs
       report_block_queue_size_ = TBSYS_CONFIG.getInt(CONF_SN_NAMESERVER, CONF_REPORT_BLOCK_MAX_QUEUE_SIZE, report_block_thread_nums * 2);
       if (report_block_queue_size_ < report_block_thread_nums * 2)
          report_block_queue_size_ = report_block_thread_nums * 2;
-      if (report_block_queue_size_ > report_block_thread_nums * 4)
-        report_block_queue_size_ = report_block_thread_nums * 4;
       const char* report_hour_str = TBSYS_CONFIG.getString(CONF_SN_NAMESERVER, CONF_REPORT_BLOCK_HOUR_RANGE, "2~4");
 
       set_hour_range(report_hour_str, report_block_time_lower_, report_block_time_upper_);
 
       choose_target_server_random_max_nums_ = TBSYS_CONFIG.getInt(CONF_SN_NAMESERVER,CONF_CHOOSE_TARGET_SERVER_RANDOM_MAX_NUM, 32);
+
+      keepalive_queue_size_ = TBSYS_CONFIG.getInt(CONF_SN_NAMESERVER, CONF_HEART_MAX_QUEUE_SIZE, 1024);
       return TFS_SUCCESS;
     }
 
@@ -239,9 +239,9 @@ namespace tfs
         = config.getInt(CONF_SN_DATASERVER, CONF_EXPIRE_CHECKBLOCK_TIME, 86400);
       max_cpu_usage_ = config.getInt(CONF_SN_DATASERVER, CONF_MAX_CPU_USAGE, 60);
       dump_stat_info_interval_ = config.getInt(CONF_SN_DATASERVER, CONF_DUMP_STAT_INFO_INTERVAL, 60000000);
-      object_dead_max_time_ = config.getInt(CONF_SN_DATASERVER, CONF_OBJECT_DEAD_MAX_TIME, 86400);
+      object_dead_max_time_ = config.getInt(CONF_SN_DATASERVER, CONF_OBJECT_DEAD_MAX_TIME, 3600);
       if (object_dead_max_time_ <=  0)
-        object_dead_max_time_ = 86400;
+        object_dead_max_time_ = 3600;
       object_clear_max_time_ = config.getInt(CONF_SN_DATASERVER, CONF_OBJECT_CLEAR_MAX_TIME, 300);
       if (object_clear_max_time_ <= 0)
         object_clear_max_time_ = 300;
